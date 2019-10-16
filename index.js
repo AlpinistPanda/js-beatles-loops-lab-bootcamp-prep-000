8b6d7ebd306e2e1476307755f688aaa8a5a4eb3e
Ozgun Balaban <ozgunbalaban@yahoo.com>
2019-10-16 22:34:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 function theBeatlesPlay(musician, instruments) {
-  var array = new Array();
+  var array[];
   for(var i = 0; i++; i < musician.length) {
     array.push(`${musician[i]} plays ${instruments[i]}`);
   }
