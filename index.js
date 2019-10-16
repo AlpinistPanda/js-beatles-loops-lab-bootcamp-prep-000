67bfc235fe64f5c927ab9da9ed2f6a81d994a4fd
Ozgun Balaban <ozgunbalaban@yahoo.com>
2019-10-16 22:33:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 function theBeatlesPlay(musician, instruments) {
   var array;
   for(var i = 0; i++; i < musician.length) {
-    array.push(musician[i]);
+    array.push(`${musician[i]} plays ${instruments[i]}`);
   }
-  return 0
+  return array
   
 }
